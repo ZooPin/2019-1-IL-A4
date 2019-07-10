68afbebcaef17af84442ddff5e36c9e37b94a270
Super_Frelon <truc5200@gmail.com>
2019-07-10 10:12:19 +0200

    Add RandomNodeGenerator

---
+++ WorkIL/ITI.Genetic/RandomNodeGenerator.cs
@@ -0,0 +1,12 @@
+using ITI.Analyzer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI.Genetic
+{
+    class RandomNodeGenerator
+    {
+        Node _node = new Node();
+    }
+}
