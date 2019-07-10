cebecc64b5ac8878fe8065962fbcd1d75c6c8927
Pingouin <me@pingouin.io>
2019-07-10 12:38:06 +0200

    Add RandomNodeGenerator.

--- WorkIL/ITI.Genetic/RandomNodeGenerator.cs
+++ WorkIL/ITI.Genetic/RandomNodeGenerator.cs
@@ -1,12 +1,93 @@
 using ITI.Analyzer;
+using ITI.Tokenizer;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ITI.Genetic
 {
-    class RandomNodeGenerator
+    public class RandomNodeGenerator
     {
-        Node _node = new Node();
+        private readonly Random _r = new Random();
+        private readonly string[] _identifier = {"x", "y"};
+
+        public Node Node()
+        {
+            switch(_r.Next(0, 5))
+            {
+                case 0: return BinaryNode();
+                case 1: return ConstantNode();
+                case 2: return IfNode();
+                case 3: return UnaryNode();
+                case 4: return IdentifierNode();
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        private Node ConstantOrIdentifierNode()
+        {
+            if (_r.Next(0, 100) > 70)
+            {
+                return ConstantNode();
+            }
+            return IdentifierNode();
+
+        }
+
+        private Node BinaryOrIdentifierOrConstantNode()
+        {
+            var r = _r.Next(0,100);
+            if (r > 70)
+            {
+                return BinaryNode();
+            }
+            
+            if ( r < 70 && r > 40)
+            {
+                return IdentifierNode();
+            }
+
+            return ConstantNode();
+        }
+
+        public BinaryNode BinaryNode()
+        {
+            return new BinaryNode(Operation(), ConstantOrIdentifierNode(), ConstantOrIdentifierNode());
+        }
+
+        public ConstantNode ConstantNode()
+        {
+            return new ConstantNode(_r.NextDouble()*100);
+        }
+
+        public IfNode IfNode()
+        {
+            return new IfNode(BinaryOrIdentifierOrConstantNode(), BinaryOrIdentifierOrConstantNode(), BinaryOrIdentifierOrConstantNode());
+        }
+
+        public UnaryNode UnaryNode()
+        {
+            return new UnaryNode(TokenType.Minus, BinaryOrIdentifierOrConstantNode());
+        }
+
+        public IdentifierNode IdentifierNode()
+        {
+            return new IdentifierNode(_identifier[_r.Next(0, _identifier.Length-1)]);
+        }
+        private TokenType Operation()
+        {
+            switch(_r.Next(0, 4))
+            {
+                case 0: return TokenType.Plus;
+                case 1: return TokenType.Minus;
+                case 2: return TokenType.Mult;
+                case 3: return TokenType.Div;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+
     }
 }
